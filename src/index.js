a9d13c7ccb12fe1be0655c5496b4bb0d10f6ad84
Belastotski <di.belost@gmail.com>
2021-02-17 02:59:15 +0100

    feat: impl function toReadable

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ module.exports =
     let ones=['','one ','two ','three ','four ','five ','six ','seven ','eight ','nine '];
     let tens=['','','twenty ','thirty ','forty ','fifty ','sixty ','seventy ','eighty ','ninety '];
     let teens=['ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-    // let big = ['thousand ', 'million ', ' billion ', ' trillion ', ' quadrillion ', ' quintillion ']
 
     return number.toString().split('').reduce((str,val,i,arr) => {
-        if (arr.length == 1 && +val == 0) return 'zero '
-        if ((arr.length - i) % 3 == 2 )
-             str += +val == 1 ? teens[+arr[i+1]]: tens[val]
-        if ((arr.length - i) % 3 == 1 )
-             str += arr.length != 1 && +arr[i-1] == 1? '' : ones[+val]
-        if ((arr.length - i) % 3 == 0)
-            str += arr.length != 1 && +val == 0? '': ones[+val] + 'hundred '
-        return str
-    },str = '').slice(0,-1)
+        switch ((arr.length - i) % 3) {
+            case 0 : return str += arr.length !== 1 && +val === 0? '': ones[+val] + 'hundred '
+            case 1 : return str += arr.length !== 1 && +arr[i-1] === 1? ''
+                :arr.length === 1 && +val === 0? 'zero ': ones[+val]
+            case 2 : return str += +val === 1 ? teens[+arr[i+1]]: tens[val]
+        }
+    },'').slice(0,-1)
 
 }
